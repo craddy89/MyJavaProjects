35578219b5c415c9c40b52d5e38bd1824a67c50b
craddy89 <yaksun63@gmail.com>
2021-06-14 17:10:38 +0300

    UPD

--- src/main/java/XML/DomParser.java
+++ src/main/java/XML/DomParser.java
@@ -1,14 +1,19 @@
 package XML;
 
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class DomParser {
@@ -19,31 +24,68 @@ public class DomParser {
             // Создается дерево DOM документа из файла
             Document document = documentBuilder.parse("C:\\Users\\yaksu\\IdeaProjects\\SkillBox\\src\\main\\java\\XML\\BookCatalog.xml");
 
-            // Получаем корневой элемент
-            Node root = document.getDocumentElement();
-
-            System.out.println("List of books:");
-            System.out.println();
-            // Просматриваем все подэлементы корневого - т.е. книги
-            NodeList books = root.getChildNodes();
-            for (int i = 0; i < books.getLength(); i++) {
-                Node book = books.item(i);
-                // Если нода не текст, то это книга - заходим внутрь
-                if (book.getNodeType() != Node.TEXT_NODE) {
-                    NodeList bookProps = book.getChildNodes();
-                    for (int j = 0; j < bookProps.getLength(); j++) {
-                        Node bookProp = bookProps.item(j);
-                        // Если нода не текст, то это один из параметров книги - печатаем
-                        if (bookProp.getNodeType() != Node.TEXT_NODE) {
-                            System.out.println(bookProp.getNodeName() + ":" + bookProp.getChildNodes().item(0).getTextContent());
-                        }
-                    }
-                    System.out.println("===========>>>>");
-                }
-            }
+            // Вызываем метод для добавления новой книги
+            addNewBook(document);
 
         } catch (ParserConfigurationException | SAXException | IOException ex) {
             ex.printStackTrace(System.out);
+        }
+    }
+
+    // Функция добавления новой книги и записи результата в файл
+    private static void addNewBook(Document document) throws TransformerFactoryConfigurationError, DOMException {
+        // Получаем корневой элемент
+        Node root = document.getDocumentElement();
+
+        // Создаем новую книгу по элементам
+        // Сама книга <Book>
+        Element book = document.createElement("Book");
+        // <Title>
+        Element title = document.createElement("Title");
+        // Устанавливаем значение текста внутри тега
+        title.setTextContent("Incredible book about Java");
+        // <Author>
+        Element author = document.createElement("Author");
+        author.setTextContent("Saburov Anton");
+        // <Date>
+        Element date = document.createElement("Date");
+        date.setTextContent("2015");
+        // <ISBN>
+        Element isbn = document.createElement("ISBN");
+        isbn.setTextContent("0-06-999999-9");
+        // <Publisher>
+        Element publisher = document.createElement("Publisher");
+        publisher.setTextContent("Java-Course publisher");
+        // <Cost>
+        Element cost = document.createElement("Cost");
+        cost.setTextContent("499");
+        // Устанавливаем атрибут
+        cost.setAttribute("currency", "RUB");
+
+        // Добавляем внутренние элементы книги в элемент <Book>
+        book.appendChild(title);
+        book.appendChild(author);
+        book.appendChild(date);
+        book.appendChild(isbn);
+        book.appendChild(publisher);
+        book.appendChild(cost);
+        // Добавляем книгу в корневой элемент
+        root.appendChild(book);
+
+        // Записываем XML в файл
+        writeDocument(document);
+    }
+
+    // Функция для сохранения DOM в файл
+    private static void writeDocument(Document document) throws TransformerFactoryConfigurationError {
+        try {
+            Transformer tr = TransformerFactory.newInstance().newTransformer();
+            DOMSource source = new DOMSource(document);
+            FileOutputStream fos = new FileOutputStream("C:\\Users\\yaksu\\IdeaProjects\\SkillBox\\src\\main\\java\\XML\\other.xml");
+            StreamResult result = new StreamResult(fos);
+            tr.transform(source, result);
+        } catch (TransformerException | IOException e) {
+            e.printStackTrace(System.out);
         }
     }
 }
